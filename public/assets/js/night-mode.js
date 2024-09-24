962ad62982cebcadf09896862910edfb6f3e672a
leehai1107 <leehai1107@gmail.com>
2024-09-24 07:17:28 +0700

    update: fix conflict

--- public/assets/js/night-mode.js
+++ public/assets/js/night-mode.js
@@ -1,21 +1,4 @@
 // Night Mode JS
-(function (window, document, undefined) {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
->>>>>>> dev
-  if (!("localStorage" in window)) return;
-  var nightMode = localStorage.getItem("gmtNightMode");
-  if (nightMode) {
-    document.documentElement.className += " night-mode";
-  }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-})(window, document);
 
 (function (window, document, undefined) {
   // Feature test
@@ -40,67 +23,3 @@
     false
   );
 })(window, document);
-=======
-	'use strict';
-	if (!('localStorage' in window)) return;
-	var nightMode = localStorage.getItem('gmtNightMode');
-	if (nightMode) {
-		document.documentElement.className += ' night-mode';
-	}
-=======
->>>>>>> main
->>>>>>> dev
-})(window, document);
-
-(function (window, document, undefined) {
-  // Feature test
-  if (!("localStorage" in window)) return;
-
-  // Get our newly insert toggle
-  var nightMode = document.querySelector("#night-mode");
-  if (!nightMode) return;
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	// Feature test
-	if (!('localStorage' in window)) return;
-
-	// Get our newly insert toggle
-	var nightMode = document.querySelector('#night-mode');
-	if (!nightMode) return;
-
-	// When clicked, toggle night mode on or off
-	nightMode.addEventListener('click', function (event) {
-		event.preventDefault();
-		document.documentElement.classList.toggle('night-mode');
-		if (document.documentElement.classList.contains('night-mode')) {
-			localStorage.setItem('gmtNightMode', true);
-			return;
-		}
-		localStorage.removeItem('gmtNightMode');
-	}, false);
-
-})(window, document);
->>>>>>> main
-=======
->>>>>>> dev
-  // When clicked, toggle night mode on or off
-  nightMode.addEventListener(
-    "click",
-    function (event) {
-      event.preventDefault();
-      document.documentElement.classList.toggle("night-mode");
-      if (document.documentElement.classList.contains("night-mode")) {
-        localStorage.setItem("gmtNightMode", true);
-        return;
-      }
-      localStorage.removeItem("gmtNightMode");
-    },
-    false
-  );
-})(window, document);
-<<<<<<< HEAD
-=======
->>>>>>> main
->>>>>>> dev
